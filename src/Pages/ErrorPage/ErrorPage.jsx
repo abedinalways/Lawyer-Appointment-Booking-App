4a2b287c68b17f1a96029fafc51609772c47f9f9
Sheikh Minhajul Abedin <nion.sk5067@gmail.com>
2025-04-23 02:25:04 +0600

    404 page created

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import { Link, useRouteError } from 'react-router';
+import { Link } from 'react-router';
 import Navbar from '../../Components/Navbar/Navbar';
 import Button from '../../Components/Button/Button';
-
+import './404.css'
 const ErrorPage = () => {
-  const error = useRouteError();
+  // const error = useRouteError();
   return (
     <>
       <Navbar></Navbar>
-      <div className='py-24 text-center'>
-        <h1 className='mb-8 text-7xl font-thin text-gray-900'>
-          {error?.status || 404}
-        </h1>
-        <p className='mb-3 text-xl font-bold text-gray-900 md:text-2xl'>
-          {error?.error?.message || 'Something Went Wrong!'}
-        </p>
-        <Link to={'/'}>
-        <Button label='Go To Homepage'></Button>
-        </Link>
+      <section className="page_404">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="col-sm-10 col-sm-offset-1 text-center">
+                <div className="four_zero_four_bg">
+                  <h1 className="text-center">404</h1>
+                </div>
+
+                <div className="contant_box_404">
+                  <h3 className="h2">Looks like you're lost</h3>
+
+                  <p>The page you are looking for is not available!</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </section>
+      <div className="flex justify-center md:mr-40">
+        <Link to={'/'}>
+          <Button label="Go To Homepage"></Button>
+        </Link>
+      </div>
     </>
   );
 };
