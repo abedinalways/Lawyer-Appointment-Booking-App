7ce852e839066ab9cb0967f19f3911d47ee3ae97
Sheikh Minhajul Abedin <nion.sk5067@gmail.com>
2025-04-23 07:45:46 +0600

    loader added in routes section

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { createBrowserRouter } from 'react-router';
 import Root from '../Pages/Root/Root';
 import ErrorPage from '../Pages/ErrorPage/ErrorPage';
@@ -6,39 +6,41 @@ import Home from '../Pages/Home/Home';
 import Details from '../Pages/Details/Details';
 import MyBookings from '../Pages/MyBookings/MyBookings';
 import Blogs from '../Pages/Blogs/Blogs';
-
+import Loader from '../Components/Loader/Loader';
 const router = createBrowserRouter([
   {
     path: '/',
     Component: Root,
-    hydrateFallbackElement: <span>loading.....</span>,
+    hydrateFallbackElement: (
+      <Suspense fallback={<Loader></Loader>}></Suspense>
+    ),
     errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         index: true,
         path: '/',
         Component: Home,
-        hydrateFallbackElement: (
-          <span className="loading loading-ring loading-md"></span>
-        ),
+        hydrateFallbackElement: <Loader></Loader>,
         loader: () => fetch('../data.json'),
       },
       {
         path: '/details/:id',
         Component: Details,
-        hydrateFallbackElement: <span>loading.....</span>,
+        hydrateFallbackElement: <Loader></Loader>,
         loader: () => fetch('../data.json'),
       },
       {
-        path:'/my-bookings',
+        path: '/my-bookings',
         Component: MyBookings,
       },
       {
         path: '/blogs',
-        hydrateFallbackElement:(<span className="loading loading-ball loading-xs"></span>),   
-        loader: ()=>fetch('../blogs.json'),
-        Component:Blogs,
-      }
+        hydrateFallbackElement: (
+          <span className="loading loading-ball loading-xs"></span>
+        ),
+        loader: () => fetch('../blogs.json'),
+        Component: Blogs,
+      },
     ],
   },
 ]);
